19dbb43af3775ff6f25f0f1c45c88d127584d40b
Réthalmi Dávid <david.rethalmi@gmail.com>
2018-09-01 17:25:46 +0200

    basic main cpp and Core class

---
+++ Core.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <QtCore/QObject>
+
+class Core : public QObject {
+Q_OBJECT
+
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <memory>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <QtCore/QCoreApplication>
+#include <QtCore/QCommandLineParser>
+
+#include "Core.h"
+
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
+    app.setApplicationVersion(QStringLiteral("0.1.0"));
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addOption(
+            {{QStringLiteral("i"), QStringLiteral("info")}, QStringLiteral("Get the players info")}
+    );
+
+    parser.process(app);
+
+    if (parser.isSet(QStringLiteral("i"))) {
+        std::cout << R"({"name":"drop_table_users"})" << std::endl;
+        return 0;
+    }
+
+    auto core = std::make_unique<Core>();
+
+    QString serverResponse;
+    bool go = true;
+    std::string temp;
+
+    while (go) {
+        std::getline(std::cin, temp);
+        serverResponse = QString::fromStdString(temp);
+
+        serverResponse.clear();
+    }
 }
